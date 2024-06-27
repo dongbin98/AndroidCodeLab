a002838e51218fb6c990401e88de8242683592dc
dongbin98 <yeomdongbin@naver.com>
2024-06-27 17:41:22 +0900

    LocalDB -> Memory 이관 및 한정자 구현

--- codelab-android-hilt-main/app/src/main/java/com/example/android/hilt/ui/LogsFragment.kt
+++ codelab-android-hilt-main/app/src/main/java/com/example/android/hilt/ui/LogsFragment.kt
@@ -28,7 +28,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.android.hilt.LogApplication
 import com.example.android.hilt.R
 import com.example.android.hilt.data.Log
+import com.example.android.hilt.data.LoggerDataSource
 import com.example.android.hilt.data.LoggerLocalDataSource
+import com.example.android.hilt.di.InMemoryLogger
 import com.example.android.hilt.util.DateFormatter
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -42,7 +44,8 @@ class LogsFragment : Fragment() {
     /**
      * 필드 삽입
      */
-    @Inject lateinit var logger: LoggerLocalDataSource
+    @InMemoryLogger
+    @Inject lateinit var logger: LoggerDataSource
     @Inject lateinit var dateFormatter: DateFormatter
 
     private lateinit var recyclerView: RecyclerView
