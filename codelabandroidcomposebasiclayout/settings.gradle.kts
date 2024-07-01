c629d0786ed4edd95338cff108e67cfce9acc01f
dongbin98 <yeomdongbin@naver.com>
2024-07-01 10:45:02 +0900

    24. 7. 1.(Mon) / init layout prj

---
+++ codelabandroidcomposebasiclayout/settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "codelab-android-compose-basic-layout"
+include(":app")
