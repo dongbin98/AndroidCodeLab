0d75b6045cd660fd74de95bea7b46750631979b4
dongbin98 <yeomdongbin@naver.com>
2024-07-03 11:53:28 +0900

    NavigationBar, NavigationRail, 화면 회전 대응

--- codelab-android-compose-basiclayout/app/src/main/java/com/tommyfuture/codelab_android_compose_basic_layout/MainActivity.kt
+++ codelab-android-compose-basiclayout/app/src/main/java/com/tommyfuture/codelab_android_compose_basic_layout/MainActivity.kt
@@ -7,13 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -26,18 +25,28 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.overscroll
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -63,15 +72,13 @@ private val alignYourBodyData = listOf(
 )
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CodelabandroidcomposebasiclayoutTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SearchBar()
-                }
-            }
+            val windowSizeClass = calculateWindowSizeClass(activity = this)
+            MyApp(windowSizeClass)
         }
     }
 }
@@ -103,14 +110,6 @@ fun SearchBar(
             .fillMaxWidth()
             .heightIn(min = 56.dp)
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun Preview() {
-    CodelabandroidcomposebasiclayoutTheme {
-        SearchBar()
-    }
 }
 
 @Composable
@@ -151,19 +150,6 @@ fun AlignYourBodyRow(
         items(alignYourBodyData) { item ->
             AlignYourBodyElement(drawable = item.drawable, text = item.text)
         }
-    }
-}
-
-@Preview(showBackground = true, backgroundColor = 0XFFF5F0EE)
-@Composable
-fun AlignYourBodyElementPreview() {
-    CodelabandroidcomposebasiclayoutTheme {
-//        AlignYourBodyElement(
-//            text = R.string.ab1_inversions,
-//            drawable = R.drawable.exam_image1,
-//            modifier = Modifier.padding(8.dp)
-//        )
-        AlignYourBodyRow(modifier = Modifier.padding(8.dp))
     }
 }
 
@@ -215,12 +201,167 @@ fun FavoriteCollectionsGrid(
     }
 }
 
-@Preview(showBackground = true, backgroundColor = 0XFFF5F0EE)
 @Composable
-fun FavoriteCollectionCardPreview() {
-    CodelabandroidcomposebasiclayoutTheme {
-        FavoriteCollectionsGrid(
-            modifier = Modifier.padding(8.dp)
+fun HomeSection(
+    @StringRes title: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column(modifier) {
+        Text(
+            text = stringResource(id = title),
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 16.dp)
+                .padding(horizontal = 16.dp)
         )
+        content()
+    }
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .verticalScroll(rememberScrollState())
+    ) {
+        Spacer(Modifier.height(16.dp))
+        SearchBar(Modifier.padding(horizontal = 16.dp))
+        HomeSection(title = R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+        HomeSection(title = R.string.favorite_collections) {
+            FavoriteCollectionsGrid()
+        }
+        Spacer(Modifier.height(16.dp))
+    }
+}
+
+@Composable
+private fun MyNavigationBar(modifier: Modifier = Modifier) {
+    NavigationBar(
+        modifier = modifier
+    ) {
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(R.string.bottom_navigation_home)
+                )
+            },
+            selected = true,
+            onClick = {}
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(id = R.string.bottom_navigation_profile)
+                )
+            },
+            selected = false,
+            onClick = {}
+        )
+    }
+}
+
+@Composable
+private fun MyNavigationRail(modifier: Modifier = Modifier) {
+    NavigationRail(
+        modifier = modifier.padding(end = 8.dp),
+        containerColor = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Home,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(
+                        text = stringResource(R.string.bottom_navigation_home)
+                    )
+                },
+                selected = true,
+                onClick = {}
+            )
+            Spacer(Modifier.height(8.dp))
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.bottom_navigation_profile)
+                    )
+                },
+                selected = false,
+                onClick = {}
+            )
+        }
+    }
+}
+
+@Composable
+fun MyAppPortrait() {
+    CodelabandroidcomposebasiclayoutTheme {
+        Scaffold(
+            bottomBar = { MyNavigationBar() }
+        ) { padding ->
+            HomeScreen(Modifier.padding(padding))
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0XFFF5F0EE, device = "spec:width=411dp, height=891dp, orientation=portrait")
+@Composable
+fun MyAppPortraitPreview() {
+    MyAppPortrait()
+}
+
+@Composable
+fun MyAppLandScape() {
+    CodelabandroidcomposebasiclayoutTheme {
+        Row {
+            MyNavigationRail()
+            HomeScreen()
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0XFFF5F0EE, device = "spec:width=411dp, height=891dp, orientation=landscape")
+@Composable
+fun MyAppLandScapePreview() {
+    MyAppLandScape()
+}
+
+@Composable
+fun MyApp(windowSize: WindowSizeClass) {
+    when  (windowSize.widthSizeClass) {
+        WindowWidthSizeClass.Compact -> {
+            MyAppPortrait()
+        }
+        WindowWidthSizeClass.Expanded -> {
+            MyAppLandScape()
+        }
     }
 }
